f762763e2367a2b2b4e618f54dfff7b2699dba8e
Anna Lundin <aolu71339@edu.ucl.dk>
2025-08-15 13:19:19 +0200

    Movieklasse oprettet i models-mappen, dog ændret lidt i forhold til DCD. #15

---
+++ The Movies WPF app/The Movies WPF app/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Movies_WPF_app.Models
+{
+    public enum MovieGenre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Horror,
+        SciFi,
+        Romance,
+        Documentary,
+        Other
+    }
+
+    internal class Movie
+    {
+        public string Title { get; set; }
+        public TimeSpan RunTime { get; set; }
+        public MovieGenre Genre { get; set; }
+
+        public Movie(string title, TimeSpan runTime, MovieGenre genre)
+        {
+            Title = title;
+            RunTime = runTime;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} ({RunTime.Hours}h {RunTime.Minutes}m, {Genre})";
+        }
+    }
+}
