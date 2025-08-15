c8fc206dba515b41c624e1134fb4edf5b2a66dc7
Amanda <amandajuhlsilberg@hotmail.com>
2025-08-15 16:47:36 +0200

    Started making the "Arrange" part.

---
+++ The Movies WPF app/Test/Test1.cs
@@ -0,0 +1,26 @@
+﻿using The_Movies_WPF_app;
+using The_Movies_WPF_app.Models;
+namespace Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ShouldAddMovieToList()
+        {
+            // Arrange
+          
+            /* var viewModel = new MainViewModel(); //We need a viewmodel with this name, for this to work.
+            viewModel.Titel = "Test Movie";
+            viewModel.RunTime = new TimeSpan(1, 1, 1); // Runtime: 1 Hr, 1 minute, 1 second.
+            viewModel.MovieGenre = MovieGenre.Horror; // Adding the genre "Horror" 
+           
+            // if we want to add multiple genres:
+            // We need to make a list in our viewmodel named MovieGenres first.
+            // viewModel.MovieGenres.Add(MovieGenre.Action);
+            //viewModel.MovieGenres.Add(MovieGenre.Comedy);
+
+*/
+        }
+    }
+}
