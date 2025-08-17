da3b2b6a01b2e3caeab8d2120534dfe161bb36a3
Thomas Løwe Hansen <tlha71412@edu.ucl.dk>
2025-08-17 23:00:40 +0200

    Repository classes created.

---
+++ The Movies WPF app/The Movies WPF app/Repositories/FileMovieRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using The_Movies_WPF_app.Models;
+
+namespace The_Movies_WPF_app.Repositories
+{
+    // Definerer offentlig klasse, som overholder IMovieRepository
+    public class FileMovieRepository : IMovieRepository
+    {
+        // Opretter en privat variabel _filePath, som er 'readonly' (værdi kan kun tildeles én gang)
+        private readonly string _filePath;
+        // Konstruktør, der kører, når et nyt objekt af klassen skabes, om som definerer filstien
+        public FileMovieRepository()
+        {
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            _filePath = Path.Combine(documentsPath, "movies.txt");
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            string lineToWrite = movie.ToString() + Environment.NewLine;
+            File.AppendAllText(_filePath, lineToWrite);
+        }
+        // Metode, der henter alle Movie-objekter
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            // Hvis filen ikke eksisterer, skabes og returneres tom liste
+            if (!File.Exists(_filePath))
+            {
+                return Enumerable.Empty<Movie>();
+            }
+
+            string[] movieLines = File.ReadAllLines(_filePath);
+            var movies = new List<Movie>();
+
+            foreach (string line in movieLines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+                Movie movie = Movie.FromString(line);
+                movies.Add(movie);
+            }
+            return movies;
+        }
+    }
+}
---
+++ The Movies WPF app/The Movies WPF app/Repositories/IMovieRepository.cs
@@ -0,0 +1,11 @@
+﻿using The_Movies_WPF_app.Models;
+using System.Collections.Generic;
+
+namespace The_Movies_WPF_app.Repositories
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<Movie> GetAllMovies();
+        void AddMovie(Movie movie);
+    }
+}
